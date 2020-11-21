41395f44b878cebc8271fbd8a297ba1aa213e11c
DongYu <dongyu1366@gmail.com>
2020-11-21 10:02:29 +0800

    Add DB

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'news',
-        'HOST': 'localhost',
-        'PORT': 3306,
-        'USER': 'root',
-        'PASSWORD': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'news.sqlite3',
     }
 }
 
